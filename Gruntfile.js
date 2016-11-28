4cec1216b14679fd64b72c90603c153b6e63e1c7
alxe_master <alxe.master@gmail.com>
2016-11-29 01:58:23 +0300

    add nwjs wrapper in build pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,53 @@
 'use strict';
 
+var path = require('path');
+var fse = require('fs-extra');
 var grunto = require('grunto');
+var NwBuilder = require('nw-builder');
 
 module.exports = grunto(function (grunt) {
+  var DIR_CWD = __dirname;
+  var SRC_DIR = 'app';
+  var BUILD_DIR = '.tmp/build';
+  var CACHE_DIR = '.tmp/nw-cache';
+  var ENV = process.env.NODE_ENV || 'development';
+  var RELEASE_DIR = '.tmp/release-' + ENV;
+
+  grunt.registerTask('build-nw', function () {
+    const done = this.async();
+
+    fse.ensureDirSync(path.join(DIR_CWD, RELEASE_DIR));
+    fse.ensureDirSync(path.join(DIR_CWD, CACHE_DIR));
+
+    var nw = new NwBuilder({
+      buildDir: path.join(DIR_CWD, RELEASE_DIR),
+      flavor: ENV === 'production' ? 'normal' : 'sdk',
+      buildType: 'timestamped',
+      cacheDir: path.join(DIR_CWD, CACHE_DIR),
+      files: [
+        `${BUILD_DIR}/**/*`
+      ],
+      platforms: [ 'win32'/*, 'win64'*/ ]
+    });
+
+    nw.on('log',  console.log);
+
+    nw.build()
+      .then(function () {
+        console.log('all done!');
+        done();
+      }, function (err) {
+        console.error(err);
+        done(err);
+      });
+  });
 
   grunt.registerTask('default', [
     'clean:dist',
     'copy:dist',
     'less:dist',
-    'uglify:dist'
+    'build-nw'
   ]);
-
-  var SRC_DIR = 'app';
-  var BUILD_DIR = '.tmp/release';
 
   return {
     clean: {
@@ -52,24 +87,11 @@ module.exports = grunto(function (grunt) {
           }
         ],
         options: {
-          compress: true,
+          compress: false,
           sourceMap: false,
           sourceMapBasepath: SRC_DIR + '/',
           sourceMapRootpath: '/'
         }
-      }
-    },
-    uglify: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            replace: true,
-            cwd: BUILD_DIR,
-            src: [ '**/*.js' ],
-            dest: BUILD_DIR
-          }
-        ]
       }
     }
   };
