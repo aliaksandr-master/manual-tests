762651aee251fb41b2c900646a879b66963be783
alxe_master <alxe.master@gmail.com>
2016-12-02 11:26:23 +0300

    fetch questions

--- app/index.js
+++ app/index.js
@@ -3,22 +3,26 @@
 import { renderToDom } from 'tiny-component';
 import bApp from './components/b-app/b-app';
 import { getObject } from './lib/localstorage';
+import { getJSON, resolve } from './lib/request';
 
-const data = {
-  store: {
-    auth: getObject('auth') || {
-      first_name: '',
-      last_name: '',
-      group_number: 0
-    },
-    test: {
-      questions: []
+
+
+resolve({
+  questions: () => getJSON('/data/questions.json')
+}).then(({ questions }) => {
+  const data = {
+    store: {
+      auth: getObject('auth') || {
+        first_name: '',
+        last_name: '',
+        group_number: 0
+      },
+      test: {
+        questions
+      }
     }
-  }
-};
+  };
 
-console.log(data);
+  renderToDom(bApp(data), document.body);
+});
 
-const component = bApp(data);
-
-renderToDom(component, document.body);
